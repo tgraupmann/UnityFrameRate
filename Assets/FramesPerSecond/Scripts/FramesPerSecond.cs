4f09458d49b37300f48faa60bf773d63573767ab
Tim Graupmann <tgraupmann@gmail.com>
2017-01-24 08:52:30 -0800

    Initial commit of project

---
+++ Assets/FramesPerSecond/Scripts/FramesPerSecond.cs
@@ -0,0 +1,38 @@
+﻿// reference: https://docs.unity3d.com/ScriptReference/Application-targetFrameRate.html
+// reference: https://forum.unity3d.com/threads/how-can-i-display-fps-on-android-device.386250/
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FramesPerSecond : MonoBehaviour
+{
+    public Text _mTextFPS = null;
+    private int _mFramesPerSec;
+    private float _mFrequency = 1.0f;
+
+
+
+    void Start()
+    {
+        StartCoroutine(FPS());
+    }
+
+    private IEnumerator FPS()
+    {
+        while (true)
+        {
+            // Capture frame-per-second
+            int lastFrameCount = Time.frameCount;
+            float lastTime = Time.realtimeSinceStartup;
+            yield return new WaitForSeconds(_mFrequency);
+            float timeSpan = Time.realtimeSinceStartup - lastTime;
+            int frameCount = Time.frameCount - lastFrameCount;
+
+            // Display it
+            if (_mTextFPS)
+            {
+                _mTextFPS.text = string.Format("FPS: {0}", Mathf.RoundToInt(frameCount / timeSpan));
+            }
+        }
+    }
+}
